7dbb67f2235e03840bcfa02d63287f47d5adcb0a
felipespa <luisfelipespa25@gmail.com>
2024-02-29 15:18:11 -0300

    improve override_get_current_user & add new tests

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -34,7 +34,7 @@ def override_get_db():
 
 
 def override_get_current_user():
-    return {}
+    return {"username": "spara", "id": 1, "user_role": "admin"}
 
 
 app.dependency_overrides[get_db] = override_get_db
@@ -76,3 +76,41 @@ def test_read_all_authenticated(test_todo):
             "owner_id": 1,
         }
     ]
+
+
+def test_read_one_authenticated(test_todo):
+    response = client.get("/todo/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "complete": False,
+        "title": "Learn to code!",
+        "description": "Need to learn everyday",
+        "id": 1,
+        "priority": 5,
+        "owner_id": 1,
+    }
+
+
+def test_read_one_authenticated_not_found():
+    response = client.get("todo/9999")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Todo not found."}
+
+
+def test_create_todo(test_todo):
+    request_data = {
+        "complete": False,
+        "title": "New todo",
+        "description": "New todo description",
+    }
+
+    response = client.post("/todo/", json=request_data)
+    assert response.status_code == 201
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 2).first()
+
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert model.priority == request_data.get("priority")
+    assert model.complete == request_data.get("complete")
