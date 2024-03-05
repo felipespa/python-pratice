b69a8c5621cd4a9a1150e0b867dd3c78126a9b39
felipespa <luisfelipespa25@gmail.com>
2024-03-04 22:06:44 -0300

    add utils file & user tests

---
+++ tests/test_users.py
@@ -0,0 +1,20 @@
+from .utils import *
+from api.routers.users import get_db, get_current_user
+from fastapi import status
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+
+def test_return_user(test_user):
+    response = client.get("/user")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["username"] == "spara"
+
+
+def test_change_password_success(test_user):
+    response = client.put(
+        "/user/password",
+        json={"password": "testpassword", "new_password": "newpassword"},
+    )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
